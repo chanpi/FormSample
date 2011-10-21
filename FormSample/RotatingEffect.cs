a98ad087a84557f30b6044da66b05fcdaee231c6
sa.murayama <sa.murayama@gmail.com>
2011-10-21 16:14:09 +0900

    Refactoring...

--- FormSample/RotatingEffect.cs
+++ FormSample/RotatingEffect.cs
@@ -17,29 +17,28 @@ namespace FormSample
         {
             try
             {
-                //Graphics g = ctrl.CreateGraphics();
+                Bitmap doubleBufferingBmp = new Bitmap(current);        // ダブルバッファリング用画面
+                Graphics g = Graphics.FromImage(doubleBufferingBmp);    // ダブルバッファリング用画面描画用Graphics
 
-                Bitmap displayBmp = new Bitmap(current);
-                Graphics g = Graphics.FromImage(displayBmp);
                 int deltaDegree = 10;
-                SolidBrush bkBrush = new SolidBrush(System.Drawing.Color.Black);
+                SolidBrush blackBrush = new SolidBrush(System.Drawing.Color.Black);
                 Rectangle rect = new Rectangle(0, 0, current.Width, current.Height);
 
-                for (int degree = 0; degree <= 360; degree += deltaDegree)
+                for (int angle = 0; angle <= 360; angle += deltaDegree)
                 {
                     g.ResetTransform();     // リセット座標変換
-                    g.FillRectangle(bkBrush, rect);
+                    g.FillRectangle(blackBrush, rect);
 
                     System.Drawing.Drawing2D.Matrix mat = new System.Drawing.Drawing2D.Matrix();
 
-                    mat.Translate(displayBmp.Width / 2, displayBmp.Height / 2, MatrixOrder.Append);   // 原点移動
-                    mat.Rotate((float)degree);
+                    mat.Translate(doubleBufferingBmp.Width / 2, doubleBufferingBmp.Height / 2, MatrixOrder.Append);   // 原点移動
+                    mat.Rotate((float)angle);
                     g.Transform = mat;      // 座標設定
 
                     // 画像の中心が(0, 0)になるように描画
-                    g.DrawImage(current, -displayBmp.Width / 2, -displayBmp.Height / 2);
+                    g.DrawImage(current, -doubleBufferingBmp.Width / 2, -doubleBufferingBmp.Height / 2);
 
-                    ((EffectablePanel)ctrl).pictureBox.Image = displayBmp;
+                    ((EffectablePanel)ctrl).pictureBox.Image = doubleBufferingBmp;
                     //ctrl.BackgroundImage = displayBmp;
                     ctrl.Refresh();
 
@@ -47,7 +46,7 @@ namespace FormSample
                     mat.Dispose();
                 }
                 g.Dispose();
-                displayBmp.Dispose();
+                doubleBufferingBmp.Dispose();
             }
             catch(SystemException ex)
             {
